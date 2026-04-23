74115aa8d3ca4f3686160304a37eacd53832a585
kamalkumar1153- <kamalkumar1153@gmail.com>
2026-04-23 16:20:37 +0530

    Update Firebase configuration
    Updated Firebase configuration with new project details.

--- firebase.js
+++ firebase.js
@@ -1,9 +1,12 @@
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT.firebaseio.com",
-  projectId: "YOUR_PROJECT"
-};
+apiKey: "AIzaSyCQ9CNumMZQNq5ED-5oGbga6SzpZFK9il4",
+  authDomain: "neet-2027-9792f.firebaseapp.com",
+  databaseURL: "https://neet-2027-9792f-default-rtdb.firebaseio.com",
+  projectId: "neet-2027-9792f",
+  storageBucket: "neet-2027-9792f.firebasestorage.app",
+  messagingSenderId: "946709283656",
+  appId: "1:946709283656:web:51a13d83e8f0a546b5697d",
+  measurementId: "G-YNHK9L90SQ
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
