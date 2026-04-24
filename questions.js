007c77b584d13f0a318733c11aa3dec1676977cd
kamalkumar1153- <kamalkumar1153@gmail.com>
2026-04-24 16:49:57 +0530

    Create questions.js with quiz questions
    Added an array of questions with options and answers.

---
+++ questions.js
@@ -0,0 +1,19 @@
+const questions = [
+  {
+    q: "SI unit of force?",
+    options: ["Newton", "Joule", "Pascal", "Watt"],
+    answer: 0
+  },
+  {
+    q: "Speed of light?",
+    options: ["3x10^8 m/s", "1x10^6", "5x10^7", "9x10^5"],
+    answer: 0
+  },
+  {
+    q: "Heart chambers?",
+    options: ["2", "3", "4", "5"],
+    answer: 2
+  }
+
+  // 👉 Aise hi 100–500 questions add karo
+];
